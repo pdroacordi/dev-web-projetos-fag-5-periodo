b1e9347e96bd098cfe6a6d80d1658433218c9a52
Pedro Acordi <pedroacordi2@gmail.com>
2025-08-12 19:54:55 -0300

    feat: add first exercise

---
+++ project-one/src/main/java/io/acordi/projectone/ProjectOneApplication.java
@@ -0,0 +1,13 @@
+package io.acordi.projectone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOneApplication.class, args);
+	}
+
+}
